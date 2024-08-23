ddbecc881485ffa282473093420e2ff5c462bd50
Kumasuke <bearcomingx@gmail.com>
2024-08-24 00:18:32 +0800

    feat(opener): optimizes Exceptions thrown by opener

--- src/main/java/com/github/kumasuke120/excel/WorkbookAutoOpener.java
+++ src/main/java/com/github/kumasuke120/excel/WorkbookAutoOpener.java
@@ -74,7 +74,7 @@ class WorkbookAutoOpener {
         } else if (filePath == null) {
             return openByInputStream();
         } else {
-            throw new AssertionError("Shouldn't happen");
+            throw new NullPointerException();
         }
     }
 
