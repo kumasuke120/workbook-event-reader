63f023fdf561b3b1bbf114ec54bcab32d82aced4
Kumasuke <bearcomingx@gmail.com>
2025-02-07 16:07:23 +0800

    chore(util): removes unnecessary reflections

--- src/main/java/com/github/kumasuke120/excel/util/ObjectFactory.java
+++ src/main/java/com/github/kumasuke120/excel/util/ObjectFactory.java
@@ -7,7 +7,6 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
-import java.lang.reflect.Constructor;
 
 /**
  * A utility class that provides some common operations on object creation
@@ -18,17 +17,11 @@ import java.lang.reflect.Constructor;
 public class ObjectFactory<T> {
 
     private final Class<T> clazz;
-    private final Constructor<T> constructor;
     private final MethodHandle constructorHandle;
-    private final boolean useHandle;
 
-    private ObjectFactory(Class<T> clazz,
-                          Constructor<T> constructor, MethodHandle constructorHandle,
-                          boolean useHandle) {
+    private ObjectFactory(Class<T> clazz, MethodHandle constructorHandle) {
         this.clazz = clazz;
-        this.constructor = constructor;
         this.constructorHandle = constructorHandle;
-        this.useHandle = useHandle;
     }
 
     /**
@@ -41,59 +34,16 @@ public class ObjectFactory<T> {
      */
     @NotNull
     public static <T> ObjectFactory<T> buildFactory(@NotNull Class<T> clazz) {
-        try {
-            return buildFactory(clazz, true);
-        } catch (ObjectCreationException e1) {
-            try {
-                return buildFactory(clazz, false);
-            } catch (ObjectCreationException e2) {
-                e2.addSuppressed(e1);
-                throw e2;
-            }
-        }
-    }
-
-    /**
-     * Creates a new {@code ObjectFactory} for the specified class
-     *
-     * @param clazz     the class
-     * @param useHandle whether to use {@link MethodHandle} to create the object
-     * @param <T>       the type of object to be created
-     * @return a new {@code ObjectFactory} for the specified class
-     */
-    @NotNull
-    static <T> ObjectFactory<T> buildFactory(@NotNull Class<T> clazz, boolean useHandle) {
-        if (useHandle) {
-            return buildFactoryWithHandle(clazz);
-        } else {
-            return buildFactoryWithReflection(clazz);
-        }
-    }
-
-    @NotNull
-    private static <T> ObjectFactory<T> buildFactoryWithHandle(@NotNull Class<T> clazz) {
         final MethodHandles.Lookup lookup = MethodHandles.lookup();
 
         final MethodHandle constructorHandle;
         try {
             constructorHandle = lookup.findConstructor(clazz, MethodType.methodType(void.class));
         } catch (NoSuchMethodException | IllegalAccessException e) {
-            throw new ObjectCreationException("Failed to find constructor of '" + clazz + "'", e);
+            throw new ObjectCreationException("failed to find constructor of '" + clazz + "'", e);
         }
 
-        return new ObjectFactory<>(clazz, null, constructorHandle, true);
-    }
-
-    @NotNull
-    private static <T> ObjectFactory<T> buildFactoryWithReflection(@NotNull Class<T> clazz) {
-        final Constructor<T> constructor;
-        try {
-            constructor = clazz.getConstructor();
-        } catch (NoSuchMethodException e) {
-            throw new ObjectCreationException("Failed to find constructor of '" + clazz + "'", e);
-        }
-
-        return new ObjectFactory<>(clazz, constructor, null, false);
+        return new ObjectFactory<>(clazz, constructorHandle);
     }
 
     /**
@@ -102,31 +52,15 @@ public class ObjectFactory<T> {
      * @return a new instance of the object
      * @throws ObjectCreationException if failed to create the object
      */
+    @SuppressWarnings("unchecked")
     @NotNull
     public T newInstance() {
-        if (useHandle) {
-            return newInstanceWithHandle();
-        } else {
-            return newInstanceWithReflection();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private T newInstanceWithHandle() {
         try {
             return (T) constructorHandle.invoke();
         } catch (ClassCastException ce) {
             throw new AssertionError("Shouldn't happen", ce);
         } catch (Throwable e) {
-            throw new ObjectCreationException("Failed to create new instance of '" + clazz + "'", e);
-        }
-    }
-
-    private T newInstanceWithReflection() {
-        try {
-            return constructor.newInstance();
-        } catch (ReflectiveOperationException e) {
-            throw new ObjectCreationException("Failed to create new instance of '" + clazz + "'", e);
+            throw new ObjectCreationException("failed to create new instance of '" + clazz + "'", e);
         }
     }
 
--- src/test/java/com/github/kumasuke120/excel/util/ObjectFactoryTest.java
+++ src/test/java/com/github/kumasuke120/excel/util/ObjectFactoryTest.java
@@ -10,110 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class ObjectFactoryTest {
 
     @Test
-    void newInstanceHandle() {
-        final ObjectFactory<A> aHandleFactory = ObjectFactory.buildFactory(A.class, true);
+    void newInstance() {
+        final ObjectFactory<A> aHandleFactory = ObjectFactory.buildFactory(A.class);
         assertNotNull(aHandleFactory);
         final A a = aHandleFactory.newInstance();
         assertNotNull(a);
 
-        final ObjectFactory<B> bHandleFactory = ObjectFactory.buildFactory(B.class, true);
+        final ObjectFactory<B> bHandleFactory = ObjectFactory.buildFactory(B.class);
         assertNotNull(bHandleFactory);
         final B b = bHandleFactory.newInstance();
         assertNotNull(b);
 
-        final ObjectFactory<E> eHandleFactory = ObjectFactory.buildFactory(E.class, true);
+        final ObjectFactory<E> eHandleFactory = ObjectFactory.buildFactory(E.class);
         assertNotNull(eHandleFactory);
         assertThrows(ObjectCreationException.class, eHandleFactory::newInstance);
 
-        assertThrows(ObjectCreationException.class, () -> ObjectFactory.buildFactory(P.class, true));
+        assertThrows(ObjectCreationException.class, () -> ObjectFactory.buildFactory(P.class));
 
-        assertThrows(ObjectCreationException.class, () -> ObjectFactory.buildFactory(N.class, true));
-    }
-
-    @Test
-    void newInstanceReflect() {
-        final ObjectFactory<A> aHandleFactory = ObjectFactory.buildFactory(A.class, false);
-        assertNotNull(aHandleFactory);
-        final A a = aHandleFactory.newInstance();
-        assertNotNull(a);
-
-        final ObjectFactory<B> bHandleFactory = ObjectFactory.buildFactory(B.class, false);
-        assertNotNull(bHandleFactory);
-        final B b = bHandleFactory.newInstance();
-        assertNotNull(b);
-
-        final ObjectFactory<E> eHandleFactory = ObjectFactory.buildFactory(E.class, false);
-        assertNotNull(eHandleFactory);
-        assertThrows(ObjectCreationException.class, eHandleFactory::newInstance);
-
-        assertThrows(ObjectCreationException.class, () -> ObjectFactory.buildFactory(P.class, false));
-
-        assertThrows(ObjectCreationException.class, () -> ObjectFactory.buildFactory(N.class, false));
-    }
-
-
-    @Test
-    void newInstanceSpeedTest() {
-        // heat up
-        newInstanceSpeedTest(A.class, 500_000, true, true);
-        newInstanceSpeedTest(A.class, 500_000, false, true);
-
-        newInstanceSpeedTest(B.class, 500_000, true, true);
-        newInstanceSpeedTest(B.class, 500_000, false, true);
-
-        // actual test
-        newInstanceSpeedTest(A.class, 100_000, true);
-        newInstanceSpeedTest(B.class, 100_000, true);
-
-        newInstanceSpeedTest(A.class, 100_000, false);
-        newInstanceSpeedTest(B.class, 100_000, false);
-
-        newInstanceSpeedTest(A.class, 5_000_000, true);
-        newInstanceSpeedTest(B.class, 5_000_000, true);
-
-        newInstanceSpeedTest(A.class, 5_000_000, false);
-        newInstanceSpeedTest(B.class, 5_000_000, false);
-
-        newInstanceSpeedTest(A.class, 50_000_000, true);
-        newInstanceSpeedTest(B.class, 50_000_000, true);
-
-        newInstanceSpeedTest(A.class, 50_000_000, false);
-        newInstanceSpeedTest(B.class, 50_000_000, false);
-    }
-
-    private <T> void newInstanceSpeedTest(Class<T> clazz, int testCount, boolean useHandle) {
-        newInstanceSpeedTest(clazz, testCount, useHandle, false);
-    }
-
-    private <T> void newInstanceSpeedTest(Class<T> clazz, int testCount, boolean useHandle, boolean heatUp) {
-        final ObjectFactory<T> factory = ObjectFactory.buildFactory(clazz, useHandle);
-        assertNotNull(factory);
-
-        long start = System.nanoTime();
-        for (int i = 0; i < testCount; i++) {
-            final T t = factory.newInstance();
-            assertNotNull(t);
-        }
-
-        if (!heatUp) {
-            final double elapsed = (System.nanoTime() - start) / 1_000_000d;
-            System.out.printf("[%s][%s] %s times new instance cost %.3f ms\n", clazz.getSimpleName(),
-                    useHandle ? "handle" : "reflect", abbrNum(testCount), elapsed);
-        }
-
-        System.gc();
-    }
-
-    private static String abbrNum(int number) {
-        if (number < 1000) {
-            return String.valueOf(number);
-        } else if (number < 1000000) {
-            return (number / 1000) + "k";
-        } else if (number < 1000000000) {
-            return (number / 1000000) + "M";
-        } else {
-            return (number / 1000000000) + "B";
-        }
+        assertThrows(ObjectCreationException.class, () -> ObjectFactory.buildFactory(N.class));
     }
 
     public static class A {
@@ -144,6 +58,7 @@ class ObjectFactoryTest {
     @SuppressWarnings("unused")
     public static class N {
         final int n;
+
         public N(int n) {
             this.n = n;
         }
